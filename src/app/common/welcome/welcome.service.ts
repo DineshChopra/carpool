069d55e536d69b179ded2c80fcdfce8582c26551
Dinesh Chopra <dinesh1.chopra@gmail.com>
2018-06-02 13:55:47 +0530

    add push functionality

--- src/app/common/welcome/welcome.service.ts
+++ src/app/common/welcome/welcome.service.ts
@@ -6,7 +6,8 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class WelcomeService {
-  readonly BASE_URL = 'http://localhost:5000/';
+  // readonly BASE_URL = 'http://localhost:5000/';
+  readonly BASE_URL = 'https://carpool-server-39059.firebaseapp.com/';
   readonly URL = {
     notificationSubscribe: this.BASE_URL + 'notification/subscribe',
     notificationSend: this.BASE_URL + 'notification/send'
